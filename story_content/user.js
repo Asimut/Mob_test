aa31f53fc4ee15fd5cc75c7f82ec55d7c92156a9
Asimut <asimut007@gmail.com>
2021-06-01 20:07:42 +0300

    Oleg

--- story_content/user.js
+++ story_content/user.js
@@ -2,6 +2,15 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "6jwqoKkmikS":
+        Script1();
+        break;
   }
+}
+
+function Script1()
+{
+  
+document.getElementsByTagName("video")[0].setAttribute("controls", "true");
 }
 
